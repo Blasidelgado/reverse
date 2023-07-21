b2f3cf443d1250ca926eb2bc4ebf72b74c0ea8be
Blasidelgado <blasidelgado@gmail.com>
2023-07-21 15:39:18 -0300

    add cs50x solution

--- reverse.c
+++ reverse.c
@@ -10,38 +10,77 @@ int get_block_size(WAVHEADER header);
 int main(int argc, char *argv[])
 {
     // Ensure proper usage
-    // TODO #1
+    if (argc != 3)
+    {
+        printf("Invalid. Proper usage: ./reverse input.wav output.wav\n");
+        return 1;
+    }
 
     // Open input file for reading
-    // TODO #2
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("File %s could not be found.\n", argv[1]);
+        return 1;
+    }
 
     // Read header
-    // TODO #3
+    WAVHEADER buffer;
+    fread(&buffer, sizeof(buffer), 1, input);
+
+    // Check file header end pointer
+    int header_end = ftell(input) - 1;
 
     // Use check_format to ensure WAV format
-    // TODO #4
+    if (!check_format(buffer))
+    {
+        printf("Input is not a valid WAV file.\n");
+        return 1;
+    }
 
     // Open output file for writing
-    // TODO #5
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("File %s could not be found.\n", argv[2]);
+        return 1;
+    }
 
     // Write header to file
-    // TODO #6
+    fwrite(&buffer, sizeof(buffer), 1, output);
 
     // Use get_block_size to calculate size of block
-    // TODO #7
+    int block_size = get_block_size(buffer);
+
+    // Set the input file pointer to the final record
+    fseek(input, -block_size, SEEK_END);
 
     // Write reversed audio to file
-    // TODO #8
+    int *data_buffer = NULL;
+    while (ftell(input) >= header_end)
+    {
+        data_buffer = malloc(block_size);
+        fread(data_buffer, block_size, 1, input);
+        fwrite(data_buffer, block_size, 1, output);
+        fseek(input, -2 * block_size, SEEK_CUR);
+    }
+
+    // Free memory and close opened files
+    free(data_buffer);
+    fclose(input);
+    fclose(output);
+
+    return 0;
 }
 
 int check_format(WAVHEADER header)
 {
-    // TODO #4
-    return 0;
+    BYTE *format = header.format;
+    return format[0] == 'W' && format[1] == 'A' && format[2] == 'V' && format[3] == 'E';
 }
 
 int get_block_size(WAVHEADER header)
 {
-    // TODO #7
-    return 0;
+    int size = header.numChannels * (header.bitsPerSample / 8);
+    return size;
 }
